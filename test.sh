c5fc4174209721ec835a9399a9966145b2ac63ff
Alex Zimin <alex.zimin@cognitivenetworks.com>
2019-10-09 18:19:10 -0700

    docker-compose + bash fixes

--- test.sh
+++ test.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 lookup=$(dig @127.0.0.1 one.one.one.one)
 
-if [ ! -z "$(echo "$lookup" | grep 'connection timed out; no servers could be reached')" ]; then
+if echo "${lookup}" | grep -q 'connection timed out; no servers could be reached'; then
   procs=$(ps aux)
   ns=$(netstat -anl)
-  echo "$procs"
-  echo "$ns"
-  echo "$lookup"
+  echo "${procs}"
+  echo "${ns}"
+  echo "${lookup}"
   exit 1
 else
-  echo "$lookup"
+  echo "${lookup}"
 fi
